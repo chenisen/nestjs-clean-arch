acf35f9f1c5d258c6cfb346664091972e86ed9bf
Marcio Chen <marcio.isaque@webhub.com.br>
2026-07-12 13:55:55 -0300

    Entity testing user

---
+++ src/users/entities/__tests__/unit/user.entity.spec.ts
@@ -0,0 +1,22 @@
+import { faker } from '@faker-js/faker';
+import { UserEntity, UserProps } from '../../user.entity';
+
+describe('UserEntity unit tests', () => {
+  let props: UserProps;
+  let sut: UserEntity;
+  beforeEach(() => {
+    props = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+    sut = new UserEntity(props);
+  });
+
+  it('Contructor method', () => {
+    expect(sut.props.name).toBe(props.name);
+    expect(sut.props.email).toBe(props.email);
+    expect(sut.props.password).toBe(props.password);
+    expect(sut.props.createdAt).toBeInstanceOf(Date);
+  });
+});
