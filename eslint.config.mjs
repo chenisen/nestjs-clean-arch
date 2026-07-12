be3805a498052968268459e2f19c3679d39a1e8e
Marcio Chen <marcio.isaque@webhub.com.br>
2026-07-12 01:43:44 -0300

    Add env config interface

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -37,7 +37,7 @@ export default defineConfig(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/interface-name-prefix': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
