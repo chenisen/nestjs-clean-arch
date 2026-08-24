29b4849a9852736ad6dd41e5f4c176f79f7daa19
Marcio Chen <marcio.isaque@webhub.com.br>
2026-08-24 18:41:35 -0300

    Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,35 @@
-import { NestFactory, Reflector } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
-import { ClassSerializerInterceptor } from '@nestjs/common';
 import { applyGlobalConfig } from './global-config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('NodeJS Clean Arch')
+    .setDescription(
+      'NodeJS Rest API - NestJS, Typescript, DDD, Clean Architecture and Automated Tests',
+    )
+    .setVersion('1.0.0')
+    .addBearerAuth({
+      description: 'Set JWT to authorize the access',
+      name: 'Authorization',
+      scheme: 'Bearer',
+      type: 'http',
+      in: 'Header',
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('reference', app, document);
+
   applyGlobalConfig(app);
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
