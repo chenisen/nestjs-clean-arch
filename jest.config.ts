ceb95b064001115cb19d0fda3b1f50bede23ff5b
Marcio Chen <marcio.isaque@webhub.com.br>
2026-08-24 19:53:02 -0300

    Fix jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,7 @@
-import { pathsToModuleNameMapper } from 'ts-jest';
-import { compilerOptions } from './tsconfig.json';
-
 module.exports = {
   rootDir: '.',
   moduleFileExtensions: ['js', 'json', 'ts'],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>/',
-  }),
+  moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
   testRegex: '.*\\..*spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
--- jest.unit.config.ts
+++ jest.unit.config.ts
@@ -1,11 +1,6 @@
-import { pathsToModuleNameMapper } from 'ts-jest';
-import { compilerOptions } from './tsconfig.json';
-
 export default {
   moduleFileExtensions: ['js', 'json', 'ts'],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>/',
-  }),
+  moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
   testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
